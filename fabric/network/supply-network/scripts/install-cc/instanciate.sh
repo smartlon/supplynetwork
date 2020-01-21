cf8228b7fbf3f3c75d6b0c479ac2eed99e16b066
smartlon <782802917@qq.com>
2020-01-21 19:17:24 +0800

    fix bug

--- fabric/network/supply-network/scripts/install-cc/instanciate.sh
+++ fabric/network/supply-network/scripts/install-cc/instanciate.sh
@@ -3,6 +3,6 @@ export ORDERER_CA=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/orde
 
 export COMPOSE_PROJECT_NAME=supplynetwork
 
-peer chaincode instantiate -o orderer.example.com:7050 --tls true --cafile $ORDERER_CA -C mychannel -l golang -n supcc -v 1.0 -c '{"Args":["init"]}' -P "AND ('ProducerMSP.peer','ManufacturerMSP.peer')"  >&log.txt
+peer chaincode instantiate -o orderer.example.com:7050 --tls true --cafile $ORDERER_CA -C logchannel -l golang -n supcc -v 1.0 -c '{"Args":["init"]}' -P "AND ('ProducerMSP.peer','ManufacturerMSP.peer')"  >&log.txt
 
 cat log.txt
