279fd72fcd6b4c04b6f8d620dad7dfb6c151bfbc
smartlon <782802917@qq.com>
2020-01-21 19:14:17 +0800

    fix bug

--- fabric/network/supply-network/scripts/channel/update-anchor.sh
+++ fabric/network/supply-network/scripts/channel/update-anchor.sh
@@ -10,7 +10,7 @@ CORE_PEER_LOCALMSPID=$MSP
 CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/$ORG.example.com/peers/$PEER.$ORG.example.com/tls/ca.crt
 CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/$ORG.example.com/users/Admin@$ORG.example.com/msp
 CORE_PEER_ADDRESS=$PEER.$ORG.example.com:$PORT
-CHANNEL_NAME=mychannel
+CHANNEL_NAME=logchannel
 CORE_PEER_TLS_ENABLED=true
 peer channel update -o orderer.example.com:7050 -c $CHANNEL_NAME -f ./channel-artifacts/${CORE_PEER_LOCALMSPID}anchors.tx --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA >&log.txt
 cat log.txt
