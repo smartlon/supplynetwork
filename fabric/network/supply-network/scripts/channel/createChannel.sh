80ab746b3431d310fbbd39ed0bc09410e25ab495
smartlon <782802917@qq.com>
2020-01-21 19:00:28 +0800

    fix bug

--- fabric/network/supply-network/scripts/channel/createChannel.sh
+++ fabric/network/supply-network/scripts/channel/createChannel.sh
@@ -5,7 +5,7 @@ CORE_PEER_LOCALMSPID=ProducerMSP
 CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/producer.example.com/peers/peer0.producer.example.com/tls/ca.crt
 CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/producer.example.com/users/Admin@producer.example.com/msp
 CORE_PEER_ADDRESS=peer0.producer.example.com:7051
-CHANNEL_NAME=mychannel
+CHANNEL_NAME=logchannel
 CORE_PEER_TLS_ENABLED=true
 ORDERER_SYSCHAN_ID=syschain
 peer channel create -o  orderer.example.com:7050 -c $CHANNEL_NAME -f ./channel-artifacts/channel.tx --tls $CORE_PEER_TLS_ENABLED --cafile $ORDERER_CA >&log.txt
@@ -16,4 +16,4 @@ cat log.txt
 
 echo 
 echo "Channel created, joining Producer..."
-peer channel join -b mychannel.block
+peer channel join -b logchannel.block
