8391501d42a47703d61d6bcbe2e20588cfb67013
smartlon <782802917@qq.com>
2020-02-06 22:19:41 +0800

    fix bug

---
+++ fabric/network/chaincode/supcc/go.mod
@@ -0,0 +1,3 @@
+module github.com/smartlon/supplynetwork/fabric/network/chaincode/supcc
+
+go 1.12
--- fabric/network/network.sh
+++ fabric/network/network.sh
@@ -12,9 +12,9 @@ if [ "$1" == "start" ]; then
     echo "       Installing chaincodes       "
     echo "***********************************"
     ./supply-network/scripts/install-cc.sh
-    echo "***********************************"
-    echo "       Registering users           "
-    echo "***********************************"
+    #echo "***********************************"
+    #echo "       Registering users           "
+    #echo "***********************************"
     #./supply-network/scripts/register-users.sh
 elif [ "$1" == "stop" ]; then
     ./supply-network/scripts/stop.sh
