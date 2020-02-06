e6ec4017398e86c92a2bbf84f0cc9aa961bcb4be
smartlon <782802917@qq.com>
2020-02-06 23:35:17 +0800

    fix bg

--- fabric/network/supply-network/scripts/install-cc/instanciate.sh
+++ fabric/network/supply-network/scripts/install-cc/instanciate.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
-export ORDERER_CA=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+PEER=$1
+ORG=$2
+MSP=$3
+PORT=$4
+VERSION=$5
+
+ORDERER_CA=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+CORE_PEER_LOCALMSPID=$MSP
+CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/$ORG.example.com/peers/$PEER.$ORG.example.com/tls/ca.crt
+CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/$ORG.example.com/users/Admin@$ORG.example.com/msp
+CORE_PEER_ADDRESS=$PEER.$ORG.example.com:$PORT
+CHANNEL_NAME=mychannel
+CORE_PEER_TLS_ENABLED=true
+#export ORDERER_CA=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
 
 export COMPOSE_PROJECT_NAME=supplynetwork
 
