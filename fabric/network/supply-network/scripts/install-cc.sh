c372d3d90e153a0e509f9f9abfc228fb6e6777fd
smartlon <782802917@qq.com>
2020-02-06 23:56:14 +0800

    fix bug

--- fabric/network/supply-network/scripts/install-cc.sh
+++ fabric/network/supply-network/scripts/install-cc.sh
@@ -10,6 +10,10 @@ echo "Installing chaincode for retailer..."
 docker exec -it cli ./scripts/install-cc/install-peer.sh peer0 retailer RetailerMSP 11051 1.0
 echo "Instanciating the chaincode..."
 docker exec -it cli ./scripts/install-cc/instanciate.sh peer0 deliverer DelivererMSP 10051 1.0
+echo "Instanciating the chaincode..."
 docker exec -it cli ./scripts/install-cc/instanciate.sh peer0 retailer RetailerMSP 11051 1.0
-docker exec -it cli ./scripts/install-cc/instanciate.sh peer0 producer ProducerMSP 7051 1.0
+echo "Instanciating the chaincode..."
 docker exec -it cli ./scripts/install-cc/instanciate.sh peer0 manufacturer ManufacturerMSP 9051 1.0
+echo "Instanciating the chaincode..."
+docker exec -it cli ./scripts/install-cc/instanciate.sh peer0 producer ProducerMSP 7051 1.0
+
